4483796288d4a87040baf73a0cf24ecc98b5aef2
353162889 <353162889@qq.com>
2018-08-03 19:30:24 +0800

    添加shader序列帧工具以及shader

---
+++ FrameSync/Assets/Editor/PictureCombine.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class PictureCombine {
+
+    [MenuItem("Assets/Custom/CombineSameSizePicture[png,OneLine]")]
+    public static void CombinePicture()
+    {
+        object[] objs = Selection.objects;
+        if(objs != null)
+        {
+            List<Texture2D> lst = new List<Texture2D>();
+            for (int i = 0; i < objs.Length; i++)
+            {
+                if(objs[i] is Texture2D)
+                {
+                    var tex = objs[i] as Texture2D;
+                    lst.Add(tex);
+                    TextureImporter ti = (TextureImporter)TextureImporter.GetAtPath(AssetDatabase.GetAssetPath(tex));
+                    ti.isReadable = true;
+                    AssetDatabase.ImportAsset(AssetDatabase.GetAssetPath(tex));
+                }
+            }
+            if (lst.Count > 0)
+            {
+                int width = 0;
+                int height = 0;
+                int perWidth = lst[0].width;
+                int perHeight = lst[0].height;
+                for (int i = 0; i < lst.Count; i++)
+                {
+                    if(perWidth != lst[i].width || perHeight != lst[i].height)
+                    {
+                        Debug.LogError("图片"+lst[i].name+"的宽高和其他图片的宽高不相等，合并失败");
+                        return;
+                    }
+                    width += lst[i].width;
+                    if (lst[i].height > height) height = lst[i].height;
+                }
+                Texture2D tex = new Texture2D(width, height);
+                for (int i = 0; i < lst.Count; i++)
+                {
+                    Color32[] colors = lst[i].GetPixels32(0);
+                    tex.SetPixels32(i * perWidth, 0, lst[i].width, lst[i].height, colors);
+                }
+                tex.Apply();
+                //保存图片
+                var bytes = tex.EncodeToPNG();
+                string path = AssetDatabase.GetAssetPath(lst[0]);
+                path = path.Replace("Assets/", "");
+                int lastIndex = path.LastIndexOf("/");
+                path = path.Substring(0, lastIndex);
+                if (path.StartsWith("/")) path = path.Substring(1);
+                string dir = Application.dataPath + "/" + path + "/CombinePicture/";
+                string file = dir + "combine_"+DateTime.Now.ToString("HH_mm_ss") + ".png";
+                Debug.Log("生成路径:"+ file);
+                if(!Directory.Exists(dir))
+                {
+                    Directory.CreateDirectory(dir);
+                }
+                File.WriteAllBytes(file, bytes);
+                AssetDatabase.Refresh();
+            }
+        }
+    }
+}
---
+++ FrameSync/Assets/ResourceEx/Shader/FrameAnim.shader
@@ -0,0 +1,89 @@
+﻿Shader "Custom/Unlit/FrameAnim" {
+	Properties{
+		_MainTex("Base (RGB)", 2D) = "white" {}               //序列帧动画纹理  
+		_Color("Color Tint", Color) = (1, 1, 1, 1)            //颜色
+		_HorizontalAmount("Horizontal Amount", float) = 6        // 行数
+		_VerticalAmount("Vertical Amount", float) = 1            // 列数 
+		_Speed("Speed", Range(1, 100)) = 7                     // 播放速度 
+	}
+
+	CGINCLUDE
+
+	#include "UnityCG.cginc"
+
+	sampler2D _MainTex;
+
+	uniform half4 _MainTex_ST;
+	uniform fixed4 _Color;
+	uniform float _HorizontalAmount;
+	uniform float _VerticalAmount;
+	uniform float _Speed;
+
+	struct appdata
+	{
+		float4 vertex : POSITION;   //从形似寄存器中读取顶点坐标
+		float2 uv : TEXCOORD0;      //从形似寄存器中读取uv坐标信息
+	};
+
+
+	struct v2f
+	{
+		float2 uv : TEXCOORD0;
+		float4 vertex : SV_POSITION;
+	};
+
+	v2f vert(appdata v)
+	{
+		v2f o;
+
+		//mul(UNITY_MATRIX_MVP,*)' == 'UnityObjectToClipPos(*) 
+		//（物体的模型到世界矩阵 * 从世界到摄像机的矩阵 * 投影的矩阵 ）
+		o.vertex = UnityObjectToClipPos(v.vertex);
+
+		//TRANSFORM_TEX,就是将模型顶点的uv和Tiling、offset两个变量进行计算得出实际显示用的定点uv
+		o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+
+		return o;
+	}
+
+	fixed4 frag(v2f i) : SV_Target
+	{
+		//所经过的时间,Unity内置变量_Timefloat4 _Time : Time (t/20, t, t*2, t*3)
+		float time = floor(_Time.y * _Speed);
+		//该时间渲染序列帧动画指定的行列
+		float row = floor(time / _HorizontalAmount);
+		float col = time - row * _HorizontalAmount;
+
+		//将所显示的单张图片缩放到应有的大小区域
+		half2 uv = float2(i.uv.x / _HorizontalAmount, i.uv.y / _VerticalAmount);
+		//移动到对应缩放后的区域位置
+		uv.x += col / _HorizontalAmount;
+		uv.y -= row / _VerticalAmount;
+		fixed4 color = tex2D(_MainTex, uv);
+		color.rgb *= _Color.rgb;
+		return color;
+	}
+
+	ENDCG
+
+	SubShader {
+	Tags{ "Queue" = "Transparent" "RenderType" = "Transparent"}
+		Cull Back
+		ZWrite Off
+		Blend SrcAlpha OneMinusSrcAlpha
+
+		Pass{
+
+		CGPROGRAM
+
+		#pragma vertex vert
+		#pragma fragment frag
+		#pragma fragmentoption ARB_precision_hint_fastest 
+
+		ENDCG
+
+		}
+
+	}
+	FallBack Off
+}
