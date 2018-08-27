59979c0358060784ea723cd7aa289c5be5bf6b78
353162889 <353162889@qq.com>
2018-08-27 22:38:12 +0800

    添加音效配置，与更改导表工具

--- Data/export_excel.py
+++ Data/export_excel.py
@@ -174,10 +174,20 @@ def convert_cell_to_string(first_line,cell):
         return ""
     return cell.value
 
+def convert_cell_to_bool(first_line,cell):
+	if is_cell_null(cell):
+		return "FALSE"
+	if str(cell.value) == "0":
+		return "FALSE"
+	else:
+		return "TRUE"
+	return str(cell.value)
+
 dic_xml_value = {
     "int": convert_cell_to_int,
     "float":convert_cell_to_float,
     "string":convert_cell_to_string,
+    "bool":convert_cell_to_bool,
 }
 
 def export_table_to_csharp(table,generate_csharp_dir,absolute_path = None,sheet_name = None):
@@ -289,10 +299,27 @@ def csharp_parse_string_property(first_line,key):
         return temp_str
     raise Exception("first_line = {0}的字符串编写错误".format(first_line))
 
+def csharp_parse_bool_property(first_line,key):
+    if first_line == "required" or first_line == "required_key":
+        return "\t\t\t{0} = bool.Parse(node.Attribute(\"{1}\"));".format(key, key)
+    elif first_line == "repeated":
+        temp_str = "\t\t\t{0} = new List<bool>();\n".format(key)
+        temp_str += "\t\t\tstring[] {0}Arr = node.Attribute(\"{1}\").Split(\',\');\n".format(key,key)
+        temp_str += "\t\t\tif ({0}Arr != null || {1}Arr.Length > 0)\n".format(key,key)
+        temp_str += "\t\t\t{\n"
+        temp_str += "\t\t\t\tfor (int i = 0; i < {0}Arr.Length; i++)\n".format(key)
+        temp_str += "\t\t\t\t{\n"
+        temp_str += "\t\t\t\t\t{0}.Add(bool.Parse({1}Arr[i]));\n".format(key,key)
+        temp_str += "\t\t\t\t}\n"
+        temp_str += "\t\t\t}"
+        return temp_str
+    raise Exception("first_line = {0}的字符串编写错误".format(first_line))
+
 dic_csharp_parse_value = {
     "int": csharp_parse_int_property,
     "float":csharp_parse_float_property,
     "string":csharp_parse_string_property,
+    "bool":csharp_parse_bool_property,
 }
 
 def csharp_convert_key_to_string(first_line,type,key):
@@ -327,10 +354,20 @@ def csharp_string_property(key,first_line):
         return "public List<string> " + key + " { get; private set; }"
     raise Exception("first_line = {0}的字符串编写错误".format(first_line))
 
+def csharp_bool_property(key,first_line):
+    if first_line == "required":
+        return "public bool "+key+" { get; private set; }"
+    elif first_line == "required_key":
+        return "[ResCfgKey]\n\t\tpublic bool "+key+" { get; private set; }"
+    elif first_line == "repeated":
+        return "public List<bool> " + key + " { get; private set; }"
+    raise Exception("first_line = {0}的字符串编写错误".format(first_line))
+
 dic_csharp_parse_key = {
     "int": csharp_int_property,
     "float":csharp_float_property,
     "string":csharp_string_property,
+    "bool":csharp_bool_property,
 }
 
 if __name__ == "__main__":
